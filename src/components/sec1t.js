6fb79781b965e452c7e5449417776712cba71490
hermes-4 <ebenezeroforia08@gmail.com>
2024-02-09 13:13:28 +0000

    Create React App

---
+++ src/components/sec1t.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Whoami = () => {
+    return (
+        <div className="whoami">
+            <h3>WHO AM I</h3>
+            {/* Add a question mark which is a graphic */}
+        </div>
+    );
+};
---
+++ src/components/sec2t.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Myname = () => {
+    return (
+        <div className="myname">
+            <p>My Name Is EBENEZER OFORI ACQUAH</p>
+        </div>
+    );
+};
