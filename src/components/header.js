cf8b0936e7c6116d80c2021d1e12a6e516b90e78
hermes-4 <ebenezeroforia08@gmail.com>
2024-02-12 21:23:52 +0000

    Create a new react app with files for different sections and their images

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,9 @@
 import React from "react";
+import "../styles/style.css";
+import me from "../images/me.png";
 
-export const Header = () => {
-    return (
-        <div className="headsect">
-            <div className="custom-bg d-flex align-items-center justify-content-center text-xl">
-                <h2>MY PORTFOLIO</h2>
-            </div>
-            <hr />
-            <div className="whoami">
-                <h2>Welcome to My Website</h2>
-                {/* Add a question mark which is a graphic */}
-            </div>
-            <div className="about">
-                <h3>About Me</h3>
-                <p>
-                    Hello there! My name is Ebenezer Ofori Acquah. I'm a
-                    passionate individual journeying through my late teens,
-                    exploring the wonders of life and learning. While my age may
-                    remain a mystery, what's clear is my dedication to growth
-                    and knowledge.
-                </p>
-            </div>
-            <div className="edu">
-                <h3> Educational Journey</h3>
-                <p>
-                    My academic journey began at Odorgonno Senior High School,
-                    where I laid the foundation for my future endeavors.
-                    Currently, I am pursuing my dreams at the prestigious
-                    University of Ghana, diving deep into the realm of Computer
-                    Science.
-                </p>
-            </div>
-            <div>
-                <h3>Join Me on the Journey</h3>
-                <p>
-                    Join me as I navigate through the complexities of academia,
-                    unravel the mysteries of technology, and embrace the
-                    challenges that shape my future. Together, let's embark on a
-                    journey of growth, discovery, and endless possibilities.
-                </p>
-            </div>
-        </div>
-    );
+const Header = () => {
+    return <div className="doc"></div>;
 };
+
+export default Header;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Header } from "../components/header";
+import Header from "../components/header";
 
-export const Homepage = () => (
+const Homepage = () => (
     <div>
         <Header />
     </div>
 );
+
+export default Homepage;
